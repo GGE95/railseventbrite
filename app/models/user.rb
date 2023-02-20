7d5bc6223f86351a22fb4728959ad21d4758eded
ghislain <genet.ghislain@gmail.com>
2023-02-20 23:42:37 +0100

    commit7

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,13 @@ class User < ApplicationRecord
   has_many :attendances
   has_many :events, through: :attendances
   has_many :created_events, class_name: 'Event', foreign_key: 'administrator_id'
-
+  validates :email, presence: true
   after_create :welcome_send
 
   def welcome_send
     UserMailer.welcome_email(self).deliver_now
   end
+
+  
 
 end
