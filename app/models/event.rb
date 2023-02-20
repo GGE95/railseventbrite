f05c0b6b97a45a4a4cbd5cd5723f75fbed39f832
ghislain <genet.ghislain@gmail.com>
2023-02-20 23:34:24 +0100

    commit5

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,14 +2,14 @@ class Event < ApplicationRecord
   has_many :attendances
   has_many :users, through: :attendances
   belongs_to :administrator, class_name: 'User', foreign_key: 'administrator_id'
-  validates :title, presence: true, length: {in: 5..140}
-  validates :description, presence: true, length: {in: 20..1000}
-  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000, message: "Le tarif doit être compris entre 1 et 1000 €" }
-  validates :location, presence: true
-  validates :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :multiple_of_five
-  validates :start_date, presence: true
-  validates :date_cannot_be_in_past
+  validate :title, presence: true, length: {in: 5..140}
+  validate :description, presence: true, length: {in: 20..1000}
+  validate :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000, message: "Le tarif doit être compris entre 1 et 1000 €" }
+  validate :location, presence: true
+  validate :duration, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validate :multiple_of_five
+  validate :start_date, presence: true
+  validate :date_cannot_be_in_past
 
   private
 
